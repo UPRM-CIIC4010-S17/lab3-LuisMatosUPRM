58e2e78c757694b30d83d775b662c01b6ab3680a
Luis Angel <Luis Angel@DESKTOP-C3810KI>
2017-12-06 18:00:53 -0400

    Question 8 push...

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -20,16 +22,53 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
+                        //int ovalsize = 55;
  
-                        //Paint the background
-                        g.setColor(Color.WHITE);
-                        g.fillRect(x1, y1, width+1, height+1);
+                          //Paint the background
+                          g.setColor(Color.BLACK);
+                          g.fillRect(x1, y1, width+1, height+1);
                         
-                        //Draw a border
-                        g.setColor(Color.RED);
-                        g.drawRect(x1, y1, width, height);
-                        
-                        g.setColor(Color.ORANGE);
-                        g.drawRect(x1+3, y1+3, width-6, height-6);
+//                        //Draw a border
+//                        g.setColor(Color.RED);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        g.setColor(Color.ORANGE);
+//                        g.drawRect(x1+3, y1+3, width-6, height-6);
+//                        
+//                        g.setColor(Color.GREEN);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        g.setColor(Color.DARK_GRAY);
+//                        g.drawLine(x2, y1, x1, y2);
+//                          
+//                        g.setColor(Color.LIGHT_GRAY);
+//                        g.fillOval(x1+(width/2)-(ovalsize/2), y1+(height/2)-(ovalsize/2), ovalsize, ovalsize);
+//                          
+//                          Polygon p = new Polygon();
+//                          p.addPoint(x1 + 5, y1 + 25);
+//                          p.addPoint(x1 + 20, y1 + 10);
+//                          p.addPoint(x1 + 35, y1 + 25);
+//                          p.addPoint(x1 + 25, y1 + 25);
+//                          p.addPoint(x1 + 25, y1 + 45);
+//                          p.addPoint(x1 + 15, y1 + 45);
+//                          p.addPoint(x1 + 15, y1 + 25);
+//                          g.setColor(Color.YELLOW);
+//                          g.fillPolygon(p);
+                          
+                          Polygon p2 = new Polygon();
+                          p2.addPoint(x1 + 25, y1 + 73);
+                          p2.addPoint(x1 + 41, y1 + 73);
+                          p2.addPoint(x1 + 47, y1 + 58);
+                          p2.addPoint(x1 + 53, y1 + 73);
+                          p2.addPoint(x1 + 69, y1 + 73);
+                          p2.addPoint(x1 + 56, y1 + 83);
+                          p2.addPoint(x1 + 61, y1 + 98);
+                          p2.addPoint(x1 + 47, y1 + 88);
+                          p2.addPoint(x1 + 34, y1 + 98);
+                          p2.addPoint(x1 + 38, y1 + 83);
+                          g.setColor(Color.WHITE);
+                          g.drawPolygon(p2);
+                          
+                          
             }
 }
