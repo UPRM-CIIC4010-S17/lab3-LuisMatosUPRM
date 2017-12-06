1720ef0686c93e653f3695e42e2ac9a218893eb2
Luis Angel <Luis Angel@DESKTOP-C3810KI>
2017-12-06 17:30:44 -0400

    *Name Change *Size Change *Location now base on your monitor *Blah, blah, blah...

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,21 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		
+		//Finds the current screen size
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		
+		JFrame myFrame = new JFrame("Luis A. Matos Velez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation((int)(width/2)-100, (int)(height/2)-100); //Uses the (int) of the located values: ((height/width)/2)-100 to center it on any monitor.
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
+	
 }
