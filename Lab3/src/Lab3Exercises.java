8b957226895e578e5b191cc632d05a2b8b8e0f98
Luis Angel <Luis Angel@DESKTOP-C3810KI>
2017-12-06 18:43:15 -0400

    Final Push

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -10,11 +10,13 @@ public class Lab3Exercises {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double width = screenSize.getWidth();
 		double height = screenSize.getHeight();
+		int ww = 210;
+		int hh = 150;
 		
 		JFrame myFrame = new JFrame("Luis A. Matos Velez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation((int)(width/2)-100, (int)(height/2)-100); //Uses the (int) of the located values: ((height/width)/2)-100 to center it on any monitor.
-		myFrame.setSize(200, 200);
+		myFrame.setLocation((int)(width/2)-(ww/2), (int)(height/2)-(hh/2)); //Uses the (int) of the located values: ((height/width)/2)-100 to center it on any monitor.
+		myFrame.setSize(ww, hh);
 		
         MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -11,6 +11,9 @@ public class MyPanelClass extends JPanel {
 	 */
 	private static final long serialVersionUID = 7057541440811488699L;
 
+			/* (non-Javadoc)
+			 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+			 */
 			public void paintComponent(Graphics g) {
                         super.paintComponent(g);
  
@@ -22,52 +25,106 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
-                        //int ovalsize = 55;
+                        
  
                           //Paint the background
-                          g.setColor(Color.BLACK);
+                          g.setColor(Color.WHITE);
                           g.fillRect(x1, y1, width+1, height+1);
-                        
-//                        //Draw a border
-//                        g.setColor(Color.RED);
-//                        g.drawRect(x1, y1, width, height);
-//                        
-//                        g.setColor(Color.ORANGE);
-//                        g.drawRect(x1+3, y1+3, width-6, height-6);
-//                        
-//                        g.setColor(Color.GREEN);
-//                        g.drawLine(x1, y1, x2, y2);
-//                        
-//                        g.setColor(Color.DARK_GRAY);
-//                        g.drawLine(x2, y1, x1, y2);
-//                          
-//                        g.setColor(Color.LIGHT_GRAY);
-//                        g.fillOval(x1+(width/2)-(ovalsize/2), y1+(height/2)-(ovalsize/2), ovalsize, ovalsize);
-//                          
-//                          Polygon p = new Polygon();
-//                          p.addPoint(x1 + 5, y1 + 25);
-//                          p.addPoint(x1 + 20, y1 + 10);
-//                          p.addPoint(x1 + 35, y1 + 25);
-//                          p.addPoint(x1 + 25, y1 + 25);
-//                          p.addPoint(x1 + 25, y1 + 45);
-//                          p.addPoint(x1 + 15, y1 + 45);
-//                          p.addPoint(x1 + 15, y1 + 25);
-//                          g.setColor(Color.YELLOW);
-//                          g.fillPolygon(p);
+                          
+                      
+/*                        int ovalsize = 55;
+*                         //Draw a border
+*                         g.setColor(Color.RED);
+*                         g.drawRect(x1, y1, width, height);
+*                        
+*                         g.setColor(Color.ORANGE);
+*                         g.drawRect(x1+3, y1+3, width-6, height-6);
+*                        
+*                         g.setColor(Color.GREEN);
+*                         g.drawLine(x1, y1, x2, y2);
+*                        
+*                         g.setColor(Color.DARK_GRAY);
+*                         g.drawLine(x2, y1, x1, y2);
+*                          
+*                         g.setColor(Color.LIGHT_GRAY);
+*                         g.fillOval(x1+(width/2)-(ovalsize/2), y1+(height/2)-(ovalsize/2), ovalsize, ovalsize);
+*                          
+*                         Polygon p = new Polygon();
+*                         p.addPoint(x1 + 5, y1 + 25);
+*                         p.addPoint(x1 + 20, y1 + 10);
+*                         p.addPoint(x1 + 35, y1 + 25);
+*                         p.addPoint(x1 + 25, y1 + 25);
+*                         p.addPoint(x1 + 25, y1 + 45);
+*                         p.addPoint(x1 + 15, y1 + 45);
+*                         p.addPoint(x1 + 15, y1 + 25);
+*                         g.setColor(Color.YELLOW);
+*                         g.fillPolygon(p);
+*                         
+*                         Polygon p2 = new Polygon();
+*                         p2.addPoint(x1 + 25, y1 + 73);
+*                         p2.addPoint(x1 + 41, y1 + 73);
+*                         p2.addPoint(x1 + 47, y1 + 58);
+*                         p2.addPoint(x1 + 53, y1 + 73);
+*                         p2.addPoint(x1 + 69, y1 + 73);
+*                         p2.addPoint(x1 + 56, y1 + 83);
+*                         p2.addPoint(x1 + 61, y1 + 98);
+*                         p2.addPoint(x1 + 47, y1 + 88);
+*                         p2.addPoint(x1 + 34, y1 + 98);
+*                         p2.addPoint(x1 + 38, y1 + 83);
+*                         g.setColor(Color.WHITE);
+*                         g.drawPolygon(p2);
+ */
+                          Polygon R1 = new Polygon();
+                          R1.addPoint(x1, y1);
+                          R1.addPoint(x1 + width +1, y1);
+                          R1.addPoint(x1 + width +1, y1 + (height/5));
+                          R1.addPoint(x1, y1 + (height/5));
+                          g.setColor(Color.RED);
+                          g.fillPolygon(R1);  
+                          
+                          Polygon R2 = new Polygon();
+                          R2.addPoint(x1, y1 + (height/5)*2);
+                          R2.addPoint(x1 + width +1, y1 + (height/5)*2);
+                          R2.addPoint(x1 + width +1, y1 + (height/5)*3);
+                          R2.addPoint(x1, y1 + (height/5)*3);
+                          g.setColor(Color.RED);
+                          g.fillPolygon(R2);
+                          
+                          Polygon R3 = new Polygon();
+                          R3.addPoint(x1, y1 + (height/5)*4);
+                          R3.addPoint(x1 + width +1, y1 + (height/5)*4);
+                          R3.addPoint(x1 + width +1, y1 + 1 +(height/5)*5);
+                          R3.addPoint(x1, y1 + 1 +(height/5)*5);
+                          g.setColor(Color.RED);
+                          g.fillPolygon(R3);
+                          
+                          Polygon arrow = new Polygon();
+                          arrow.addPoint(x1, y1);
+                          arrow.addPoint(x1 + (width/2)+5, y1 + (height/2));
+                          arrow.addPoint(x1, y1 + height);
+                          g.setColor(Color.BLUE);
+                          g.fillPolygon(arrow);                          
+                          
+                          double wc = 1.3;
+                          double hc = 1.5;
                           
                           Polygon p2 = new Polygon();
-                          p2.addPoint(x1 + 25, y1 + 73);
-                          p2.addPoint(x1 + 41, y1 + 73);
-                          p2.addPoint(x1 + 47, y1 + 58);
-                          p2.addPoint(x1 + 53, y1 + 73);
-                          p2.addPoint(x1 + 69, y1 + 73);
-                          p2.addPoint(x1 + 56, y1 + 83);
-                          p2.addPoint(x1 + 61, y1 + 98);
-                          p2.addPoint(x1 + 47, y1 + 88);
-                          p2.addPoint(x1 + 34, y1 + 98);
-                          p2.addPoint(x1 + 38, y1 + 83);
+                          p2.addPoint((int) (x1 + 25/wc), (int) (y1 + 73/hc));
+                          p2.addPoint((int) (x1 + 41/wc), (int) (y1 + 73/hc));
+                          p2.addPoint((int) (x1 + 47/wc), (int) (y1 + 58/hc));
+                          p2.addPoint((int) (x1 + 53/wc), (int) (y1 + 73/hc));
+                          p2.addPoint((int) (x1 + 69/wc), (int) (y1 + 73/hc));
+                          p2.addPoint((int) (x1 + 56/wc), (int) (y1 + 83/hc));
+                          p2.addPoint((int) (x1 + 61/wc), (int) (y1 + 98/hc));
+                          p2.addPoint((int) (x1 + 47/wc), (int) (y1 + 88/hc));
+                          p2.addPoint((int) (x1 + 34/wc), (int) (y1 + 98/hc));
+                          p2.addPoint((int) (x1 + 38/wc), (int) (y1 + 83/hc));
                           g.setColor(Color.WHITE);
-                          g.drawPolygon(p2);
+                          g.fillPolygon(p2);
+                          
+                          
+                          
+                          
                           
                           
             }
